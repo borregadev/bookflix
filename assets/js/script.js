9aac49b82cce08f7455ccd51cfcea13a6924c465
JoeCross <krstevskigj@gmail.com>
2024-01-18 21:57:50 +0000

    complete  new search function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 //we are waiting first for the whole DOM to load
-$(document).ready(()=> {
+$(document).ready(async ()=> {
     //calling the initial search result, which will display the newest books published instad search.
     searchResults();
+
+    //just testing
+    console.log(await getNewestBooks());
 });
--- assets/js/search_results.js
+++ assets/js/search_results.js
@@ -47,3 +47,5 @@ function searchResults() {
         searchBox.append(row);
     }
 }
+
+
